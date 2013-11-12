688e1679d0ebdef8a706746f99f1a1a0778bac1a
Marv Cool <marv@hostin.is>
2013-11-12 18:59:27 +0100

    refactore-moves EveauthHelpers into own file

--- lib/rack/eveauth.rb
+++ lib/rack/eveauth.rb
@@ -5,47 +5,7 @@ require_relative '../backend/backend'
 
 module Rack
 
-  module EveauthHelper
-
-    class IGBRequest
-      def initialize(request)
-        @env = request
-      end
-
-      def method_missing(m)
-        @env["HTTP_EVE_"+m.to_s.upcase]
-      end
-
-      def trusted?
-        @env['HTTP_EVE_TRUSTED'] == "Yes"
-      end
-    end
-
-    def auth_with_password(name,pass)
-      c = ::Eveauth::Capsuleer.where({name: name}).first
-      if c.is_this_your_password?(pass)
-        auth!(c)
-        redirect "/"
-      else
-        halt 403
-      end
-    end
-
-    def auth!(capsuleer)
-      capsuleer.authenticated=true
-      session[:srmt] = capsuleer.secret_remember_me_token!
-    end
-
-    def register!(pass)
-      if not ::Eveauth::Capsuleer.where({name: session[:capsuleer].name}).exists?
-        session[:capsuleer].password = pass
-        session[:capsuleer].authenticated = true
-        auth!(session[:capsuleer])
-      end
-      session[:capsuleer].save
-    end
-
-  end
+  require_relative 'eveauth_helper'
 
   class Eveauth < Sinatra::Base
     enable :sessions
